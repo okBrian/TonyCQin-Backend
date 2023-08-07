99514cc665e1316738c826aeaada7f7952f57121
Brian Ok <brianok924@gmail.com>
2023-08-07 18:42:35 -0400

    deleted repeated files

--- transfer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Set the paths and IP addresses
-LOCAL_PATH="."
-RASPBERRY_PI_IP="169.254.177.9"
-RASPBERRY_PI_PATH="/home/brian/Desktop/pog"
-
-# Transfer files using SCP
-scp -r $LOCAL_PATH/* brian@$RASPBERRY_PI_IP:$RASPBERRY_PI_PATH
